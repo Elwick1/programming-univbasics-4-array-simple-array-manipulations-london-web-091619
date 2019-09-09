ff13ba73e2b73a03fc282608ffceaa1683aa8e51
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 08:42:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -57,7 +57,7 @@ end
     #expect(instructors).not_to include("Steven")
     
    def using_delete(instructors)
-     instructors.delete
+     instructors.delete(1)
    end
     
   #  describe "using_delete_at" do 
