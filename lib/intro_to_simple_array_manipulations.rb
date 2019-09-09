5b20ee2e42e7b92c216a502b7e8352accf538e55
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 08:47:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,18 +26,6 @@ end
      my_favorite_things.concat(more_favs) 
    end
   
-  
-  
- # describe "using_insert" do 
-  #before(:each) do 
-   # @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-    #@another_language = "Python"
-    #@new_array = using_insert(@list_of_programming_languages, @another_language)
-#  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
- #   expect(@new_array[4]).to eq(@another_language)
-  #it "increases the length of the array" do 
-   # expect(@new_array.length).to eq(8)
-    
   def using_insert(list_of_programming_languages, another_language)
     list_of_programming_languages.insert(4, another_language)
   end
@@ -50,12 +38,6 @@ end
     instruments.flatten
   end
   
-  #describe "using_delete" do 
-  #it "takes in two arguments, an array and a string, and uses the delete method to remove any items from the #array that are equal to that string" do
-    #instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    #no_offense_steven = using_delete(instructors, "Steven")
-    #expect(instructors).not_to include("Steven")
-    
    def using_delete(instructors, no_offense_steven)
      instructors.delete("Steven")
    end
