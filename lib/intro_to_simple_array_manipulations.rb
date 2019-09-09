3b2008536809d5e45fa1b836f36b900a4d128cfa
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 08:24:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,9 +31,9 @@ end
    # expect(@all_my_favs).to eq(["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"])
   # it "increases the length of the first array" do 
    # expect(@my_favorite_things.length).to be > 2
-  # def using_concat(my_favorite_things, more_favs)
-   #  .concat 
-   #end
+   def using_concat(my_favorite_things, more_favs)
+     .concat 
+   end
   
   
   
